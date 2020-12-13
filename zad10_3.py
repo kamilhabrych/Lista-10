f9c18634a92549a4d5e7a0b739a921f3802985c2
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 18:27:35 +0100

    Create dicts

---
+++ zad10_3.py
@@ -0,0 +1,13 @@
+sl1 = {
+    'Color' : 'Black',
+    'Color' : 'White',
+    'Color' : 'Green',
+    'Color' : 'Red'
+}
+
+sl2 = {
+    'Animal' : 'Dog',
+    'Animal' : 'Cat',
+    'Animal' : 'Dog',
+    'Animal' : 'Turtle'
+}
