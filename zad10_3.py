2f64f8df7f8b25d8d582926ef83db792dbcfd9c3
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 18:51:48 +0100

    Final init

--- zad10_3.py
+++ zad10_3.py
@@ -1,13 +1,25 @@
+def bezpowt(sl):
+    if len(sl) == len(set(sl)):
+        return 1
+    else:
+        return 0
+
 sl1 = {
-    'Color' : 'Black',
-    'Color' : 'White',
-    'Color' : 'Green',
-    'Color' : 'Red'
+    'Color1': 'Black',
+    'Color2': 'White',
+    'Color3': 'Green',
+    'Color4': 'Red'
 }
 
 sl2 = {
-    'Animal' : 'Dog',
-    'Animal' : 'Cat',
-    'Animal' : 'Dog',
-    'Animal' : 'Turtle'
+    'Animal1': 'Dog',
+    'Animal2': 'Cat',
+    'Animal3': 'Dog',
+    'Animal4':'Turtle'
 }
+
+l1 = list(sl1.values())
+l2 = list(sl2.values())
+
+print(bezpowt(l1))
+print(bezpowt(l2))
