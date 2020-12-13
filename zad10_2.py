ad4684e3f24d53e8dc68c917aa1d0c31de3c4a0f
Kamil Habrych <habrychkamil@gmail.com>
2020-12-13 18:20:34 +0100

    Rename variable

--- zad10_2.py
+++ zad10_2.py
@@ -1,12 +1,12 @@
 l1 = ['First Name', 'Last Name', 'Age', 'Birthday']
 l2 = ['Kamil', 'Habrych', '22', '24-08-1998']
 
-_dict = {}
+dict = {}
 
 for key in l1:
     for value in l2:
-        _dict[key] = value
+        dict[key] = value
         l2.remove(value)
         break
 
-print(_dict)
+print(dict)
