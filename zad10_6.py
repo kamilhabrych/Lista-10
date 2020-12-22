a717f5104f03a3a7044cab6146f1172ba302eced
Kamil Habrych <habrychkamil@gmail.com>
2020-12-22 15:04:01 +0100

    Task complete

--- zad10_6.py
+++ zad10_6.py
@@ -2,8 +2,8 @@ dict1 = {}
 
 with open('dictionary.txt', 'r', encoding='UTF8') as f:
     for line in f:
-        (key, val) = line.rstrip("\n").split(':')
-        dict1[key] = val
+        (key, value) = line.rstrip("\n").split(':')
+        dict1[key] = value
 
     f.close()
 
